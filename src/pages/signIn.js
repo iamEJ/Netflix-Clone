732c3e07b652c83aa8dd9fa698be811a7e75bf5d
iamEJ <jukneviciute185@gmail.com>
2020-10-28 14:26:37 +0200

    setup and style sign in form

--- src/pages/signIn.js
+++ src/pages/signIn.js
@@ -1,5 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { HeaderContainer } from "../containers/header";
+import { FooterContainer } from "../containers/footer";
+import Form from "../components/form";
+import * as ROUTES from "../constants/routes";
 
 export default function SignIn() {
-  return <h1>sign in</h1>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+  };
+  const isInvalid = password === "" || email === "";
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignIn} method="POST">
+            <Form.Input
+              placeholder="Enter email address"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Form.Input
+              placeholder="Enter password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Form.Submit type="submit" disabled={isInvalid}>
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+          <Form.Text>
+            New to Netflix?{" "}
+            <Form.Link to={ROUTES.SIGN_UP}>Sign up now.</Form.Link>{" "}
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. Learn more.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
