7fb854919965616cd53844efe53f9335a2c5e5b5
iamEJ <jukneviciute185@gmail.com>
2020-10-29 19:43:35 +0200

    setup browse header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,18 @@
 import React from "react";
-import { Background, Container, Frame, Button, Logo } from "./style/header";
+import {
+  Background,
+  Container,
+  Button,
+  Logo,
+  Feature,
+  Text,
+  FeatureCallOut,
+  TextLink,
+  Group,
+  Picture,
+  Profile,
+  Dropdown,
+} from "./style/header";
 import { Link as ReactRouterLink } from "react-router-dom";
 
 export default function Header({ bg = true, children, ...restProps }) {
@@ -8,6 +21,46 @@ export default function Header({ bg = true, children, ...restProps }) {
 
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
+};
+
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+  return <Group {...restProps}>{children}</Group>;
+};
+
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+  return <Feature {...restProps}>{children}</Feature>;
+};
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
+  return <TextLink {...restProps}>{children}</TextLink>;
+};
+
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+  return <Profile {...restProps}>{children}</Profile>;
+};
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+  return <Dropdown {...restProps}>{children}</Dropdown>;
+};
+
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+  return (
+    <Picture
+      {...restProps}
+      src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/64623a33850498.56ba69ac2a6f7.png"
+    />
+  );
+};
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({
+  children,
+  ...restProps
+}) {
+  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
 };
 
 Header.Logo = function HeaderLogo({ to, ...restProps }) {
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -2,6 +2,9 @@ import React, { useContext, useState, useEffect } from "react";
 import { SelectProfileContainer } from "./profiles";
 import { FirebaseContext } from "../context/firebase";
 import Loading from "../components/loading";
+import Header from "../components/header";
+import * as ROUTES from "../constants/routes";
+import { auth } from "../lib/firebase.prod";
 
 export function BrowseContainer() {
   const { firebaseApp } = useContext(FirebaseContext);
@@ -17,11 +20,50 @@ export function BrowseContainer() {
   }, [profile.displayName]);
 
   return profile.displayName ? (
-    loading ? (
-      <Loading src={user.photoURL} />
-    ) : (
-      <Loading.ReleaseBody />
-    )
+    <>
+      {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+      <Header src="joker1" dontShowOnSmallScreans>
+        <Header.Frame>
+          <Header.Group>
+            <Header.Logo
+              src="https://www.brandvia.com/wp-content/uploads/2019/04/Netflix-Logo.png"
+              to={ROUTES.HOME}
+              alt="Netflix"
+            ></Header.Logo>
+            <Header.TextLink>Series</Header.TextLink>
+            <Header.TextLink>Films</Header.TextLink>
+          </Header.Group>
+          <Header.Group>
+            <Header.Profile>
+              <Header.Picture src={user.photoURL} />
+              <Header.Dropdown>
+                <Header.Group>
+                  <Header.Picture src={user.photoURL} />
+                  <Header.TextLink>{user.displayName}</Header.TextLink>
+                </Header.Group>
+                <Header.Group>
+                  <Header.TextLink
+                    to={ROUTES.HOME}
+                    onClick={() => auth.signOut()}
+                  >
+                    Logout
+                  </Header.TextLink>
+                </Header.Group>
+              </Header.Dropdown>
+            </Header.Profile>
+          </Header.Group>
+        </Header.Frame>
+        <Header.Feature>
+          <Header.FeatureCallOut>Watch Now: The Joker</Header.FeatureCallOut>
+          <Header.Text>
+            In Gotham City, mentally troubled comedian Arthur Fleck is
+            disregarded and mistreated by society. He then embarks on a downward
+            spiral of revolution and bloody crime. This path brings him
+            face-to-face with his alter-ego: the Joker.
+          </Header.Text>
+        </Header.Feature>
+      </Header>
+    </>
   ) : (
     <SelectProfileContainer user={user} setProfile={setProfile} />
   );
